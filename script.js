7a021c3fdcb33399f2a313eb11e03e207ebc69d4
lie03 <139715955+lie031@users.noreply.github.com>
2024-01-12 10:15:38 -0500

    added class feature

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ const modal = document.querySelector('#modal');
 const cardContainer = document.querySelector('.card-container');
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+
+class Book{
+    constructor(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
-
-function saveLibrary(library) {
+    }
+    
+    static saveLibrary(library){
     const serializedLibrary = library.map(book => {
         return {
             title: book.title,
@@ -26,20 +28,82 @@ function saveLibrary(library) {
         };
     });
     localStorage.setItem('library', JSON.stringify(serializedLibrary));
+    }
+
+    static addBookToLibrary(title, author, pages, read) {
+        let book = new Book(title, author, pages, read);
+        myLibrary.push(book);
+        Book.saveLibrary(myLibrary);
+    }
+
+     static render(arr){
+        cardContainer.innerHTML = '';
+        arr.forEach(element => {
+            const card = document.createElement('div');
+            const titleTemp= document.createElement('p');
+            titleTemp.textContent = element.title;
+            const authorTemp = document.createElement('p');
+            authorTemp.textContent = "Author: " + element.author;
+            const pagesTemp = document.createElement('p');
+            pagesTemp.textContent = "Pages:" + element.pages;
+            const btnContainer = document.createElement('div');
+            btnContainer.classList.add('btn-container');
+            const readTemp = document.createElement('button');
+            readTemp.classList.add('btn-read');
+            readTemp.textContent = element.read? 'Read' : 'Not read';
+            const deleteBtn = document.createElement('button');
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.classList.add('btn-read');
+            card.classList.add('cardstyle');
+            card.classList.add(element.read?'read':'not-read');
+            card.appendChild(titleTemp);
+            card.appendChild(authorTemp);
+            card.appendChild(pagesTemp);
+            btnContainer.appendChild(readTemp);
+            btnContainer.appendChild(deleteBtn);
+            card.appendChild(btnContainer);
+            cardContainer.appendChild(card);
+
+            readTemp.addEventListener('click',()=>{
+                if(card.classList.contains('read')){
+                    card.classList.remove('read');
+                    card.classList.add('not-read');
+                    readTemp.textContent = 'Not read';
+                }
+                else{
+                    card.classList.remove('not-read');
+                    card.classList.add('read');
+                    readTemp.textContent = 'Read';
+                }
+            })
+
+            readTemp.addEventListener('click', () => {
+                element.read = !element.read;
+                Book.saveLibrary(myLibrary);
+                Book.render(myLibrary);
+            });
+
+            deleteBtn.addEventListener('click', () => {
+
+            const index = myLibrary.indexOf(element);
+        
+            if (index !== -1) {
+                myLibrary.splice(index, 1);
+                Book.saveLibrary(myLibrary);
+                Book.render(myLibrary);
+            }
+            });
+        });   
+    }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    saveLibrary(myLibrary);
-}
 
 window.addEventListener('load', () => {
     const storedLibrary = localStorage.getItem('library');
     if (storedLibrary) {
         const serializedLibrary = JSON.parse(storedLibrary);
         myLibrary = serializedLibrary.map(book => new Book(book.title, book.author, book.pages, book.read));
-        render(myLibrary);
+        Book.render(myLibrary);
         console.log('library loaded');
     } else {
         console.log('no library found in local storage');
@@ -54,8 +118,8 @@ addBtn.addEventListener('click', (event) => {
     const read = readInput.checked;
 
     if (title && author && pages !== '') {
-        addBookToLibrary(title, author, pages, read);
-        render(myLibrary);
+        Book.addBookToLibrary(title, author, pages, read);
+        Book.render(myLibrary);
         titleInput.value = '';
         authorInput.value = '';
         pagesInput.value = '';
@@ -76,63 +140,4 @@ modal.addEventListener('click', (event) => {
     }
 });
 
-function render(arr){
-    cardContainer.innerHTML = '';
-    arr.forEach(element => {
-        const card = document.createElement('div');
-        const titleTemp= document.createElement('p');
-        titleTemp.textContent = element.title;
-        const authorTemp = document.createElement('p');
-        authorTemp.textContent = "Author: " + element.author;
-        const pagesTemp = document.createElement('p');
-        pagesTemp.textContent = "Pages:" + element.pages;
-        const btnContainer = document.createElement('div');
-        btnContainer.classList.add('btn-container');
-        const readTemp = document.createElement('button');
-        readTemp.classList.add('btn-read');
-        readTemp.textContent = element.read? 'Read' : 'Not read';
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.classList.add('btn-read');
-        card.classList.add('cardstyle');
-        card.classList.add(element.read?'read':'not-read');
-        card.appendChild(titleTemp);
-        card.appendChild(authorTemp);
-        card.appendChild(pagesTemp);
-        btnContainer.appendChild(readTemp);
-        btnContainer.appendChild(deleteBtn);
-        card.appendChild(btnContainer);
-        cardContainer.appendChild(card);
-
-        readTemp.addEventListener('click',()=>{
-            if(card.classList.contains('read')){
-                card.classList.remove('read');
-                card.classList.add('not-read');
-                readTemp.textContent = 'Not read';
-            }
-            else{
-                card.classList.remove('not-read');
-                card.classList.add('read');
-                readTemp.textContent = 'Read';
-            }
-        })
-
-        readTemp.addEventListener('click', () => {
-            element.read = !element.read;
-            saveLibrary(myLibrary);
-            render(myLibrary);
-        });
-
-        deleteBtn.addEventListener('click', () => {
-
-            const index = myLibrary.indexOf(element);
-        
-            if (index !== -1) {
-                myLibrary.splice(index, 1);
-                saveLibrary(myLibrary);
-                render(myLibrary);
-            }
-        });
-
-    });   
-}
+     
